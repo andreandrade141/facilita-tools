5cee7580f4b127b895560badb16832bf4945a68e
André Andrade <94050052+andreandrade141@users.noreply.github.com>
2022-05-02 14:21:51 -0300

    move_files.py v2

--- move_files.py
+++ move_files.py
@@ -2,19 +2,23 @@ import os
 import shutil
 
 
-source_path = ''
-destination_path = ''
+source_path = '/home/andre/Área de Trabalho/bcklupo/02052022/'
+destination_path = '/home/andre/Área de Trabalho/bcklupo/empty/'
 
 
 def move_files(src, dest):
+    counter = 0
     files = os.listdir(source_path)
 
+
     for file in files:
-        if os.path.getsize(file) <= 50:
-            print(f'arquivo {file} menor que 50 B')
+        if os.path.getsize(f'{source_path}{file}') <= 1000:
+            print(f'arquivo {file} menor que 1 Kb')
             s = src+file
             d = dest+file
             shutil.move(s, d)
+            counter +=1
+    print('Arquivos movidos: ', counter)
 
 
 move_files(source_path, destination_path)
