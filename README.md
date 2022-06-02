7226c1145a4f04cf13c1e5748f4f21e75dabcc87
andreandrade141 <andrelsandrade43@gmail.com>
2022-06-01 22:09:18 -0300

    Adiciona input por argv

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # QoL_tools
+
 Simple tools for boring tasks :)
 
 ## Tools Descriptions:
-1) move_files_by_size -> Pretty self-explanatory... It only moves the files on a directory when it's size matches or is bigger/smaller than a value.
+
+1. move_files_by_size -> Pretty self-explanatory... It only moves the files on a directory when it's size matches or is bigger/smaller than a value. Arguments are passed in argv for, in this order: src folder, dest folder and file size.
--- move_files.py
+++ move_files.py
@@ -1,24 +1,25 @@
 import os
+import sys
 import shutil
 
 
-source_path = ''
-destination_path = ''
+source_path = sys.argv[1]
+destination_path = sys.argv[2]
+size = sys.argv[3]
 
 
-def move_files(src, dest):
+def move_files(src: str, dest: str, size: int) -> None:
     counter = 0
     files = os.listdir(source_path)
 
-
     for file in files:
-        if os.path.getsize(f'{source_path}{file}') <= 1000:
+        if os.path.getsize(f'{source_path}{file}') <= size:
             print(f'arquivo {file} menor que 1 Kb')
             s = src+file
             d = dest+file
             shutil.move(s, d)
-            counter +=1
+            counter += 1
     print('Arquivos movidos: ', counter)
 
 
-move_files(source_path, destination_path)
+move_files(source_path, destination_path, size)
