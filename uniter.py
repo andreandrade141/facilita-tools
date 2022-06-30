03c094e63090b7f9a50c1ebd2fb6bab1e4959ce3
André Andrade <94050052+andreandrade141@users.noreply.github.com>
2022-06-30 16:06:38 -0300

    Update uniter.py

--- uniter.py
+++ uniter.py
@@ -3,6 +3,10 @@ import csv
 
 from pprint import pprint
 
+PATH = ''
+HEADER = ''
+VALIDATOR = ''
+COMPARATOR = 0
 
 class Uniter:
     def __init__(self, path):
@@ -11,7 +15,7 @@ class Uniter:
     def unite(self):
         with open(os.path.join(self.path, 'output.csv'), 'a') as resultfile:
             resultfile.write(
-                'cod_loja,cod_venda,data_hora,cancelado,troca,total\n')
+                HEADER)
             for file in os.listdir(self.path):
                 if not file.endswith(".CSV"):
                     print("Skipping file: " + file)
@@ -20,11 +24,11 @@ class Uniter:
                 with open(os.path.join(self.path, file), 'r') as csvfile:
                     data = csv.reader(csvfile, delimiter=';')
                     for row in data:
-                        if row[0] == 'cod_loja':
+                        if row[COMPARATOR] == VALIDATOR:
                             continue
                         resultfile.write(str(row).replace(
                             '[', '').replace(']', '').replace("'", '')+'\n')
 
 
-unite = Uniter('/home/andre/workspace/Analise_authentic_feet')
+unite = Uniter(PATH)
 unite.unite()
